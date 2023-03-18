1dedd2834da780e7e003c329de04321abda3617e
mashru020 <mashrurrahman16@gmail.com>
2023-03-18 13:45:52 +0600

    proper created successfully

--- mongodb/models/property.js
+++ mongodb/models/property.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const PropertySchema = new mongoose.Schema({
     title: { type: String, required: true},
-    desctiption: { type: String, required: true},
+    description: { type: String, required: true},
     propertyType: { type: String, required: true},
     location: { type: String, required: true},
     price: { type: Number, requried: true},
