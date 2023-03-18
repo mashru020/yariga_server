183bec7bb320808db152e76a44ccc028dff8d235
mashru020 <mashrurrahman16@gmail.com>
2023-03-18 11:30:07 +0600

    bug solved

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express({limit: '50mb'}));
+app.use(express.json({limit: '50mb'}));
 
 app.get('/', (req, res)=> {
     res.send({message:'Hello World!'});
